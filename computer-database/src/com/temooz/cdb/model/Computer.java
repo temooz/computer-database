8edfccd3e320b18dac14e5bf8d0c71f26ae5094b
temooz <temooz@users.noreply.github.com>
2015-06-21 21:23:33 +0200

    Database model

---
+++ computer-database/src/com/temooz/cdb/model/Computer.java
@@ -0,0 +1,94 @@
+package com.temooz.cdb.model;
+
+import java.sql.Timestamp;
+
+public class Computer {
+
+	private Long id;                        
+	private String name;                     
+	private Timestamp introduced;                
+	private Timestamp discontinued;             
+	private Company companyId;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Timestamp getIntroduced() {
+		return introduced;
+	}
+	public void setIntroduced(Timestamp introduced) {
+		this.introduced = introduced;
+	}
+	public Timestamp getDiscontinued() {
+		return discontinued;
+	}
+	public void setDiscontinued(Timestamp discontinued) {
+		this.discontinued = discontinued;
+	}
+	public Company getCompany() {
+		return companyId;
+	}
+	public void setCompany(Company company_id) {
+		this.companyId = company_id;
+	}               
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((companyId == null) ? 0 : companyId.hashCode());
+		result = prime * result
+				+ ((discontinued == null) ? 0 : discontinued.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result
+				+ ((introduced == null) ? 0 : introduced.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Computer other = (Computer) obj;
+		if (companyId == null) {
+			if (other.companyId != null)
+				return false;
+		} else if (!companyId.equals(other.companyId))
+			return false;
+		if (discontinued == null) {
+			if (other.discontinued != null)
+				return false;
+		} else if (!discontinued.equals(other.discontinued))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (introduced == null) {
+			if (other.introduced != null)
+				return false;
+		} else if (!introduced.equals(other.introduced))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+}
